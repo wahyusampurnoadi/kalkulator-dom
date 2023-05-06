691e06d87617a67689399c1ce8b78115ef01e621
Wahyu Sampurno Adi <wahyusampurnoadi@gmail.com>
2023-05-06 15:44:27 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,72 @@
+// menampoung variabel
+const inputFirst = document.getElementById("input-1");
+const inputSecond = document.getElementById("input-2");
+
+const tambah = document.getElementById("plus");
+const kurang = document.getElementById("minus");
+const kali = document.getElementById("times");
+const bagi = document.getElementById("divide");
+
+const result = document.getElementById("result");
+
+// mengambil nilai input
+function getValue1() {
+  let value1 = inputFirst.value;
+  console.log(value1);
+}
+
+function getValue2() {
+  let value2 = inputSecond.value;
+  console.log(value2);
+}
+
+//membuat fungsi tambah
+function tambahkan(value1, value2) {
+  if (value1 == "" || value2 == "") {
+    return (document.getElementById("result").innerHTML = "Tidak boleh kosong!");
+  } else {
+    let hasilTambah = value1 + value2;
+    document.getElementById("result").innerHTML = hasilTambah;
+    return hasilTambah;
+  }
+}
+
+// membuat fungsi kurang
+function kurangkan(value1, value2) {
+  if (value1 == "" || value2 == "") {
+    return (document.getElementById("result").innerHTML = "Tidak boleh kosong!");
+  } else {
+    let hasilKurang = value1 - value2;
+    document.getElementById("result").innerHTML = hasilKurang;
+    return hasilKurang;
+  }
+}
+
+// membuat fungsi kali
+function kalikan(value1, value2) {
+  if (value1 == "" || value2 == "") {
+    return (document.getElementById("result").innerHTML = "Tidak boleh kosong!");
+  } else {
+    let hasilKali = value1 * value2;
+    document.getElementById("result").innerHTML = hasilKali;
+    return hasilKali;
+  }
+}
+
+// mebuat fungsi bagi
+function bagikan(value1, value2) {
+  if (value1 == "" || value2 == "") {
+    return (document.getElementById("result").innerHTML = "Tidak boleh kosong!");
+  } else {
+    let hasilBagi = value1 / value2;
+    document.getElementById("result").innerHTML = hasilBagi;
+    return hasilBagi;
+  }
+}
+
+// membuat fungsi tombol reset
+function reset() {
+  inputFirst.value = "";
+  inputSecond.value = "";
+  result.innerHTML = "";
+}
---
+++ readme.txt
@@ -0,0 +1,27 @@
+# Kalkulator Sederhana
+
+Kalkulator sederhana sering digunakan dalam berbagai situasi sehari-hari, baik untuk tujuan pendidikan, bisnis, atau keperluan pribadi. Dengan fitur-fitur yang mudah digunakan, kalkulator sederhana membantu pengguna untuk menghemat waktu dan upaya dalam melakukan perhitungan matematika secara efisien.
+
+## Deskripsi
+
+Kalkulator Sederhana adalah sebuah program komputer yang dirancang untuk melakukan operasi matematika dasar, seperti penjumlahan, pengurangan, perkalian, dan pembagian. Program ini memberikan antarmuka yang mudah digunakan bagi pengguna untuk memasukkan angka dan melakukan perhitungan matematika dengan cepat dan akurat.
+
+## Fitur
+
+Daftar fitur-fitur utama yang ada dalam proyek ini.
+
+Penjumlahan: Menghitung jumlah dari dua atau lebih angka.
+Pengurangan: Mengurangi satu angka dari angka lain.
+Perkalian: Mengalikan dua atau lebih angka.
+Pembagian: Membagi satu angka dengan angka lain.
+Tampilan hasil: Menampilkan hasil perhitungan secara jelas kepada pengguna.
+
+Dapat digunakan dalam mobile view (responsive).
+
+## Penggunaan
+
+Cukup masukkan kedua angka yang ingin Anda operasikan, pilih jenis operasi matematika yang Anda inginkan, dan Anda akan segera mendapatkan hasilnya. Tidak perlu repot menghitung secara manual, Kalkulator Sederhana akan memberikan jawaban yang akurat dalam hitungan detik. Cobalah sekarang dan nikmati kemudahan dalam melakukan perhitungan matematika sehari-hari!
+
+## Credits
+
+By Wahyu Sampurno Adi - 2023
